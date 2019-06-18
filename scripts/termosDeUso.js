20fc5b691e6883414781ecb9fd9024ff346291a9
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-18 17:57:02 -0300

    02 - termos de uso e o que é JS e o que é API do browser

---
+++ scripts/termosDeUso.js
@@ -0,0 +1,25 @@
+if(localStorage.getItem('aceitouTermos') === null){
+    // CONST é Variável. Dinâmico
+    const nome = prompt('Qual o seu nome')
+
+    const aceitouTermos = confirm(`
+        Olá ${nome}!
+
+        Antes de usar o Cake, precisamos que 
+        você aceite nossos termos de uso:
+
+        • Você aceita que este software foi 
+            feito por pessoas que participaram 
+            do curso de JavaScript.
+        • Você aceita que o código dessas 
+            pessoas pode acessar tudo o que 
+            você digitar aqui.
+    `)
+
+    if(!aceitouTermos) {
+        alert(nome + ', não podemos continuar juntos')
+        window.close()
+    } else {
+        localStorage.setItem('aceitouTermos', aceitouTermos)
+    }
+}
