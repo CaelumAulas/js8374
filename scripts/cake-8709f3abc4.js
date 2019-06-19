ba817001f05104a510e50a7c0c6b0504ab7bf189
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-19 14:24:18 -0300

    Teste para o prompt funcionar no cake windows

--- scripts/cake-8709f3abc4.js
+++ scripts/cake-8709f3abc4.js
@@ -1,10 +1,23 @@
 ;(() => {
-  window.isElectron =
-    (window &&
-      window.process &&
-      window.process.type &&
-      typeof window.process.type === 'string') ||
-    false
+  //isElectron function from: https://github.com/cheton/is-electron/blob/4acaa6cab8b527c57c6e56381e6f9766d57c56b2/index.js
+  window.isElectron = function isElectron() {
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+        return true;
+    }
+
+    // Main process
+    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+        return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to true
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+        return true;
+    }
+
+    return false;
+  }()
 })()
 
 ;(() => {
@@ -24,48 +37,47 @@
   const $iframe = document.querySelector('iframe')
 
   // let startLoadEvent = new Event('startload');
+    $iframe.addEventListener('load', function() {
+      $iframe.contentWindow.document.body.addEventListener('click', function(
+        event
+      ) {
+        event.preventDefault()
 
-  $iframe.addEventListener('load', function() {
-    $iframe.contentWindow.document.body.addEventListener('click', function(
-      event
-    ) {
-      event.preventDefault()
+        const $anchor =
+          $iframe.contentDocument.activeElement.tagName === 'A'
+            ? $iframe.contentDocument.activeElement
+            : event.path && event.path.filter
+            ? event.path.find(element => element.tagName === 'A')
+            : event.target.tagName === 'A'
+            ? event.target
+            : event.target.closest('a')
 
-      const $anchor =
-        $iframe.contentDocument.activeElement.tagName === 'A'
-          ? $iframe.contentDocument.activeElement
-          : event.path && event.path.filter
-          ? event.path.find(element => element.tagName === 'A')
-          : event.target.tagName === 'A'
-          ? event.target
-          : event.target.closest('a')
+        if ($anchor) {
+          const linkAddress = $anchor.href
 
-      if ($anchor) {
-        const linkAddress = $anchor.href
+          let endereco
 
-        let endereco
-
-        try {
-          endereco = new URL(linkAddress)
-        } catch (e) {
-          if (e instanceof TypeError) {
-            const enderecoAtual = $iframe.contentWindow.location.href.replace(
-              /\/$/,
-              ''
-            )
-            const stringEndereco =
-              enderecoAtual + '/' + linkAddress.replace(/^\//, '')
-            endereco = new URL(stringEndereco)
+          try {
+            endereco = new URL(linkAddress)
+          } catch (e) {
+            if (e instanceof TypeError) {
+              const enderecoAtual = $iframe.contentWindow.location.href.replace(
+                /\/$/,
+                ''
+              )
+              const stringEndereco =
+                enderecoAtual + '/' + linkAddress.replace(/^\//, '')
+              endereco = new URL(stringEndereco)
+            }
           }
-        }
 
-        if (typeof $iframe.onlinkclick === 'function') {
-          $iframe.onlinkclick(endereco.toString().replace(/\/$/, ''))
-        }
+          if (typeof $iframe.onlinkclick === 'function') {
+            $iframe.onlinkclick(endereco.toString().replace(/\/$/, ''))
+          }
 
-        $iframe.contentWindow.location.href = endereco
-        // $iframe.dispatchEvent(startLoadEvent)
-      }
+          $iframe.contentWindow.location.href = endereco
+          // $iframe.dispatchEvent(startLoadEvent)
+        }
+      })
     })
-  })
 })()
