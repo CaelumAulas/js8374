1a7cc60e8fef32cfaab5ea7d32efbe00f0d4acbb
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-20 12:10:35 -0300

    09 - Comecando a pagina de configuracao

---
+++ scripts/paginaConfiguracao.js
@@ -0,0 +1,5 @@
+import paginaInicial from '/scripts/storage/paginaInicial.js'
+import aceitouSalvar from '/scripts/storage/aceitouSalvar.js'
+
+$inputPaginaInicial.value = paginaInicial
+$inputPermitiuSalvar.checked = aceitouSalvar
