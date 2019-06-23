9168101c0da2c93326abce544679c000549f8865
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-23 01:31:12 -0300

    Fix usando aceitou salvar

--- scripts/pedePaginaInicial.js
+++ scripts/pedePaginaInicial.js
@@ -73,23 +73,25 @@ Type coercing
 
 */
 
-let paginaInicial = localStorage.getItem('paginaInicial')
-
-if(!paginaInicial) {
-    paginaInicial = prompt("Escolha a página inicial")
-}
-
-if(paginaInicial) {
-    if (
-        paginaInicial.substring(0, 7) !== 'http://' &&
-        paginaInicial.substring(0,8) !== 'https://'
-    ) {
-        // Assignement Atribuição
-        paginaInicial = 'http://' + paginaInicial
+if (aceitouSalvar) {
+    let paginaInicial = localStorage.getItem('paginaInicial')
+    
+    if(!paginaInicial) {
+        paginaInicial = prompt("Escolha a página inicial")
     }
-
-    $janelaPrincipal.src =  paginaInicial
-    $inputEndereco.value = paginaInicial
-
-    localStorage.setItem('paginaInicial', paginaInicial)
+    
+    if(paginaInicial) {
+        if (
+            paginaInicial.substring(0, 7) !== 'http://' &&
+            paginaInicial.substring(0,8) !== 'https://'
+        ) {
+            // Assignement Atribuição
+            paginaInicial = 'http://' + paginaInicial
+        }
+    
+        $janelaPrincipal.src =  paginaInicial
+        $inputEndereco.value = paginaInicial
+    
+        localStorage.setItem('paginaInicial', paginaInicial)
+    }
 }
