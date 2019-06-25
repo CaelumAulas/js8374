bc7e4c75c1a468487a5698ec45cd9ccdba6c8578
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-25 13:38:02 -0300

    19 - digitando na barra de endereco

--- scripts/navegacao/barraEndereco.js
+++ scripts/navegacao/barraEndereco.js
@@ -1,3 +1,5 @@
+import { carregar } from '/scripts/navegacao/carregar.js'
+import { formataEndereco } from '/scripts/endereco/formataEndereco.js'
 
 $inputEndereco.addEventListener('focus', exibeEnderecoCompleto)
 
@@ -14,3 +16,14 @@ function exibeEnderecoResumido() {
 
     $inputEndereco.value = enderecoResumido
 }
+
+// Todo parâmetro é opcional
+$inputEndereco.addEventListener('keyup', function(evento) {
+    // o navegador executou o callback
+    // as informações
+    const apertouEnter = evento.key === 'Enter'
+    if(apertouEnter) {
+        const enderecoCompleto = formataEndereco($inputEndereco.value)
+        carregar(enderecoCompleto)
+    }
+})
