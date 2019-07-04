045a62a2222fea7ea10f3fe189bc6d3e949501a8
Artur Diniz Adam <artur.daz@gmail.com>
2019-07-04 02:17:07 -0300

    Adicionando lib  para render do favoritos e do loading

--- scripts/cake-8709f3abc4.js
+++ scripts/cake-8709f3abc4.js
@@ -36,7 +36,7 @@
 ;(() => {
   const $iframe = document.querySelector('iframe')
 
-  // let startLoadEvent = new Event('startload');
+  let linkclickEvent = (endereco) => new CustomEvent('linkclick', {detail: endereco});
     $iframe.addEventListener('load', function() {
       $iframe.contentWindow.document.body.addEventListener('click', function(
         event
@@ -74,10 +74,88 @@
           if (typeof $iframe.onlinkclick === 'function') {
             $iframe.onlinkclick(endereco.toString().replace(/\/$/, ''))
           }
+          
+          if($iframe.dispatchEvent(linkclickEvent(endereco))) {
+            $iframe.contentWindow.location.href = endereco
+          }
 
-          $iframe.contentWindow.location.href = endereco
-          // $iframe.dispatchEvent(startLoadEvent)
         }
       })
     })
+})()
+
+;(() => {
+  window.$Cake = {}
+
+  const toDOMElement = (htmlString) => {
+    const dummyElement = document.createElement('p')
+    dummyElement.innerHTML = htmlString
+    return dummyElement.children[0]
+  }
+
+  const loadingTemplate = (text) => `
+    <small id="cake-loading">
+      ${text}
+    </small>
+  `
+
+  let $loading = null
+
+  window.$Cake.setLoading = function (text) {
+    $loading = toDOMElement(loadingTemplate(text))
+    document.body.appendChild($loading)
+  }
+
+  window.$Cake.removeLoading = function () {
+    if($loading !== null && $loading.remove) {
+      $loading.remove()
+    }
+  }
+
+  const $favoritosList = document.querySelector('nav')
+
+  if($favoritosList !== null) {
+    const favoritoTemplate = (favorito) => `
+      <p title="${favorito.descricao}">
+        ${favorito.nome}
+      </p>
+    `
+
+    const favorites = []
+
+    function renderFavorites() {
+      if(favorites.length === 0) {
+        $favoritosList.innerHTML = ''
+      } else {
+        const $fList = $favoritosList.innerHTML = favorites
+          .map(f => {
+            const $f = toDOMElement(favoritoTemplate(f))
+            $f.addEventListener('click', f.onclick)
+            return $f
+          })
+
+          $favoritosList.innerHTML = ''
+          for(let $newF of $fList) {
+            $favoritosList.appendChild($newF)
+            $favoritosList.appendChild(document.createElement('br'))
+          }
+      }
+    }
+
+    window.$Cake.removeFavorite = function(toRemoveF) {
+      const favorite = favorites.find(f => {
+        return f.nome === toRemoveF.nome  && f.descricao === toRemoveF.descricao
+      })
+
+      const favoritePosition = favorites.indexOf(favorite)
+
+      favorites.splice(favoritePosition, 1)
+      renderFavorites()
+    }
+    
+    window.$Cake.addFavorite = function(favorite) {
+      favorites.push(favorite)
+      renderFavorites()
+    }
+  }
 })()
